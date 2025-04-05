e3862f8cbd29431cdd66dd3e9f55a461fd7b5f77
Denish Murawala <141576236+denish178@users.noreply.github.com>
2025-04-05 16:18:04 +0530

    Update PrintSumOfN.cpp

--- Recursion/PrintSumOfN.cpp
+++ Recursion/PrintSumOfN.cpp
@@ -1,4 +1,5 @@
 // Online C++ compiler to run C++ program online
+//USING PARAMETERS IN FUNCTION
 #include <iostream>
 using namespace std;
 
@@ -16,6 +17,31 @@ int main() {
     cin >> n;
     
     int sum = doSum(n,0);
+    cout << sum << endl;
+
+    return 0;
+}
+////////////////////////////////////////////////////////////////////////////
+//METHOD-2
+//USING RECURRENCE RELATION 
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int doSum(int n){
+    
+    if(n==1) return 1;
+
+    return n+doSum(n-1);
+    
+}
+
+int main() {
+    
+    int n;
+    cin >> n;
+    
+    int sum = doSum(n);
     cout << sum << endl;
 
     return 0;
