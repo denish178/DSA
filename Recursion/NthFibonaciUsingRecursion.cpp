18e0b5a167a7eca1d27cccbe84554023e7725b94
Denish Murawala <141576236+denish178@users.noreply.github.com>
2025-04-06 11:34:44 +0530

    Create NthFibonaciUsingRecursion.cpp

---
+++ Recursion/NthFibonaciUsingRecursion.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int fibonaci(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+
+    return fibonaci(n - 1) + fibonaci(n - 2);
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int fib = fibonaci(n);
+    cout << fib << endl;
+
+    return 0;
+}
